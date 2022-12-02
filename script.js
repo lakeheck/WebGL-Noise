2baee1aaff7051d1a0e42c6d1cf9397a8d0087e7
Lake Heckaman <lakeheckaman@gmail.com>
2022-12-02 15:50:32 -0500

    img

--- script.js
+++ script.js
@@ -1107,9 +1107,9 @@ let sunraysTemp;
 let noise;
 
 //load texture for dithering
-let ditheringTexture = createTextureAsync('LDR_LLL1_0.png');
+let ditheringTexture = createTextureAsync('img/LDR_LLL1_0.png');
 // let picture = createTextureAsync('img/flowers_fence.JPG');
-let picture = createTextureAsync('img/lake-heckaman-IMG_0997-dec-2022.jpg');
+// let picture = createTextureAsync('img/lake-heckaman-IMG_0997-dec-2022.jpg');
 // console.log('loaded picture successfully');
 
 //create all our shader programs 
