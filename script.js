9e1fb05976eb5ad61a6cf449eed0d400d453ab63
Lake Heckaman <lakeheckaman@gmail.com>
2022-12-03 09:19:38 -0500

    err

--- script.js
+++ script.js
@@ -2051,7 +2051,7 @@ window.addEventListener('touchend', e => {
 window.addEventListener('keydown', e => {
     if (e.code === 'KeyP')
         config.PAUSED = !config.PAUSED;
-    if (e.key === ' ')
+    if (e.key === ' '){}
         // splatStack.push(parseInt(Math.random() * 20) + 5);
 });
 
