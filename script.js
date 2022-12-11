7085d98d03c913268e7a3bde6b6d7c5d30710c53
Lake Heckaman <lakeheckaman@gmail.com>
2022-12-11 12:16:45 -0500

    tt

--- script.js
+++ script.js
@@ -6,18 +6,9 @@ import {config} from "./js/config.js";
 import {Noise} from "./js/Noise.js";
 import * as LGL from "./js/WebGL.js";
 
-
-import {Stats} from "./stats.module.js";
-
 LGL.resizeCanvas();
-
-let stats = new Stats();
-let container = document.createElement('div')
-document.body.appendChild(container );
-container.appendChild(stats.dom);
 
 let n = new Noise();
 n.initFramebuffers();
 n.startGUI();
 n.simulate();
-stats.update();
