f6f59747be182b1ef70cf74037ee688e730981ea
Lake Heckaman <lakeheckaman@gmail.com>
2022-12-17 11:39:19 -0500

    updated mobile test

--- script.js
+++ script.js
@@ -6,7 +6,13 @@ import {config} from "./js/config.js";
 import {Noise} from "./js/Noise.js";
 import * as LGL from "./js/WebGL.js";
 
+if (LGL.isMobile()) {
+    config.DYE_RESOLUTION = 512;
+}
+
 LGL.resizeCanvas();
+
+
 
 let n = new Noise();
 n.initFramebuffers();
